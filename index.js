fad333cc267c040b2f5e2d532116dae9be20744c
Najmul-Shaon <najmul.nh.shaon@gmail.com>
2022-09-30 22:18:30 +0600

    added dom on case item

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+// phone item start
+
 // for phone plus(+) icon 
 
 
@@ -13,17 +15,49 @@ document.getElementById('btn-phone-plus').addEventListener('click', function () 
 // for phone minus(-) icon 
 
 document.getElementById('btn-phone-minus').addEventListener('click', function () {
-    // console.log('minus worked')
     const phoneQuantityCount = document.getElementById('phone-quantity-count');
     const phoneQuantityCountString = phoneQuantityCount.value;
-    phoneQuantityCountNumber = parseInt(phoneQuantityCountString);
+    const phoneQuantityCountNumber = parseInt(phoneQuantityCountString);
 
     if (phoneQuantityCountNumber > 1) {
         const newPhoneQuantityCount = phoneQuantityCountNumber - 1;
         phoneQuantityCount.value = newPhoneQuantityCount;
-        console.log('worked');
     }
-    else
+    else {
         window.alert("You cann't buy a zero quantity item.");
+    }
+})
 
+
+
+// phone item end
+
+
+
+// case item start
+
+// for + icon 
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+    const phoneQuantityCount = document.getElementById('case-quantity-count');
+    const phoneQuantityCountString = phoneQuantityCount.value;
+    phoneQuantityCountNumber = parseInt(phoneQuantityCountString);
+    const newPhoneQuantityCount = phoneQuantityCountNumber + 1;
+    phoneQuantityCount.value = newPhoneQuantityCount;
+})
+
+// for phone minus(-) icon 
+
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+    const phoneQuantityCount = document.getElementById('case-quantity-count');
+    const phoneQuantityCountString = phoneQuantityCount.value;
+    const phoneQuantityCountNumber = parseInt(phoneQuantityCountString);
+
+    if (phoneQuantityCountNumber > 1) {
+        const newPhoneQuantityCount = phoneQuantityCountNumber - 1;
+        phoneQuantityCount.value = newPhoneQuantityCount;
+    }
+    else {
+        window.alert("You cann't buy a zero quantity item.");
+    }
 })
